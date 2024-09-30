37d557118924962e82b817bd780064c92e10fb74
NANOOS105 <158564718+NANOOS105@users.noreply.github.com>
2024-09-30 10:05:08 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 32.14 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        int nn = n;
+        nn = n/2;
+        
+        for(int i=0; i < nn; i++){
+                answer += "수박";
+            }
+        
+        if(n % 2 == 1){
+            answer += "수";
+        }
+        return answer;
+    }
+}
